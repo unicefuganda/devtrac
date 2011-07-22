dafa54623b04bab9ef7660d4bcc4b83c82c4c668
Jan van Diepen <janvandiepen@195578.no-reply.drupal.org>
2011-07-22 11:22:06 +0300

    Updated features

---
+++ update.sh
@@ -0,0 +1,5 @@
+ 
+drush dis -y author_pane xmlsitemap views_galleriffic itweak_upload
+drush pm-uninstall -y colorbox admin wurfl author_pane css_injector drupalforfirebug georss itweak_upload mobileplugin nodereference_explorer textimage user_activity views_galleriffic
+drush pm-uninstall -y xmlsitemap
+drush vset --yes mobile_tools_desktop_url http://devtrac.jan

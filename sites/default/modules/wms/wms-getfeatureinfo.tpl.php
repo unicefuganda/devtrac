b3c5bf1ffc13575dedbadcadc1ea75b861eae16b
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2011-08-19 13:19:35 +0300

    Added missing file and extra div in wms tpl file

--- sites/default/modules/wms/wms-getfeatureinfo.tpl.php
+++ sites/default/modules/wms/wms-getfeatureinfo.tpl.php
@@ -5,10 +5,15 @@
  * @file
  * Theme implementation to display getfeatureinfo result
  */
-
-foreach ($tables as $name => $table) {
-  $name = str_replace(":", " ", $name);
-  print '<div class="' . $name . '">';
-  print theme_table($table['header'], $table['rows'], Array('class' => $name), $name, NULL, TRUE, t("No results"));
-  print '</div>';
-}
+print '<div class="layers">';
+if ($tables)
+  foreach ($tables as $name => $table) {
+    $name = str_replace(":", " ", $name);
+    print '<div class="' . $name . '">';
+    print theme_table($table['header'], $table['rows'], Array('class' => $name), $name, NULL, TRUE, t("No results"));
+    print '</div>';
+  }
+  else {
+    print t("No results found");
+  }
+print '</div>';

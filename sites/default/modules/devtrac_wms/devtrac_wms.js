49a3086c0dcf4a61e55eb82ca37980d4de2ff2bf
reinier <reinier@eeepc.(none)>
2011-08-22 19:21:31 +0300

    Fixed bug when clicking ' Add to Fieldtrip'  link in popup.

--- sites/default/modules/devtrac_wms/devtrac_wms.js
+++ sites/default/modules/devtrac_wms/devtrac_wms.js
@@ -5,36 +5,46 @@
 Drupal.behaviors.devtracwms = function (context) {
   $('.getfeatureaddlink:not(.wmsdevtrac-processed)', context)
     .click (function() {
-    $(this).addClass('getfeatureinfo-submitting');
-    var url = this.href;
-    
-    if (url == undefined) {
-      url = me.href;
-    }
-    if (url == undefined) {
-      return true;
-    }
-    $.ajax({
-      url: url,
-      type: 'GET',
-      success: function(response) {
-        // Call all callbacks.
-        if (response.__callbacks) {
-          $.each(response.__callbacks, function(i, callback) {
-            eval(callback)(element, response);
-          });
-        }
-        alert(response.content);
-        //$(element).html(response.content);
-      },
-      error: function(response) {
-          alert(response.content);
-          $(this).removeClass('getfeatureinfo-submitting');
-      },
-      dataType: 'json'
-    });
-    return false;
+      return Drupal.devtracwms.click(this);
     })
 
   .addClass('wmsdevtrac-processed');
 };
+
+//Initialize settings array.
+Drupal.devtracwms = {};
+
+Drupal.devtracwms.click = function(me) {
+  $(this).addClass('getfeatureinfo-submitting');
+  var url = this.href;
+  
+  if (url == undefined) {
+    url = me.href;
+  }
+  if (url == undefined) {
+    return true;
+  }
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function(response) {
+      // Call all callbacks.
+      if (response.__callbacks) {
+        $.each(response.__callbacks, function(i, callback) {
+          eval(callback)(element, response);
+        });
+      }
+      alert(response.content);
+      //$(element).html(response.content);
+    },
+    error: function(response) {
+        alert(response.content);
+        $(this).removeClass('getfeatureinfo-submitting');
+    },
+    dataType: 'json'
+  });
+  return false;
+}
+
+
+

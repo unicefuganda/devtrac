c7fd32ea7553484b90da5465028c94e6c51660c6
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2011-10-19 17:29:26 +0300

    Added javascript logic to Addplaceform

--- sites/default/themes/devtrack_theme/js/devtrac.js
+++ sites/default/themes/devtrack_theme/js/devtrac.js
@@ -1,6 +1,12 @@
 /**
  * 
  */
+$(document).ready(function() {
+  $('input[name="reporttype"]')[0].checked = true; 
+  $('input[name="placetype"]')[0].checked = true; 
+}); 
+
+
 
 Drupal.behaviors.devtrac = function (context) {
   /** Finder Page
@@ -28,11 +34,30 @@ Drupal.behaviors.devtrac = function (context) {
   $('#edit-sitereporttype:not(.processed)').each(function() {
     $(this).addClass('processed'); //this is for ourselves
     $(this).change(function() {
-      $('#edit-placetype')[0].disabled = $(this)[0].selectedIndex == 2 
+      $('#edit-placetype')[0].disabled = $(this)[0].selectedIndex == 2;
       return false;
     });
     // call change for initial settings
     $(this).change();
   });
+  
+  
 
+  /** Add place page
+   *  If adding Roadside Observation, then hide placetype 
+   */
+  $('#devtrack-module-addplaceform .form-radio[name="reporttype"]:not(.processed)').each(function() {
+    $(this).addClass('processed'); //this is for ourselves
+      $(this).change(function() {
+        $('input[name="placetype"]').attr('disabled', ($(this)[0].value == 1 && $(this)[0].checked));
+        $('input[name="placetype"]').parent().parent().parent().parent().toggle(!($(this)[0].value == 1 && $(this)[0].checked));
+        $($('input[name="placetype"]')[4]).parent().toggle(!($(this)[0].value == 0 && $(this)[0].checked));
+        if ($('input[name="placetype"]')[4].checked) {
+          $('input[name="placetype"]')[3].checked = ($(this)[0].value == 0 && $(this)[0].checked);
+        }
+        return false;
+      });
+      // call change for initial settings
+      $(this).change();
+  });
 };

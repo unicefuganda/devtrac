f099ef54d8f2f904ad5e3b6dc6b3315b0d16b2f4
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2011-11-11 13:00:55 +0300

    fixed onchange bug in IE. well not in IE, nobody can do that, but our app now behaves

---
+++ sites/default/themes/devtrack_theme/js/devtrac.js
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+// only on the addplace page, check these boxes by default
+  if (!$.browser.msie) {
+    $('input[name="reporttype"]')[0].checked = true;
+    $('input[name="placetype"]')[0].checked = true;
+  }
+});
+
+Drupal.behaviors.devtrac = function (context) {
+  /** Finder Page
+   * All items with a placetype are Site Visits.
+   */
+  $('#edit-placetype:not(.processed)').each(function() {
+    $(this).addClass('processed'); //this is for ourselves
+      // IE does not see the change event when it happens, so we help it a bit. FUCKERS!!
+      $(this).click(function() {
+        if ($.browser.msie) {
+          $(this).change();
+          $(this).blur();
+
+       }
+      });
+    $(this).change(function() {
+      if ($(this)[0].selectedIndex > 0 ) {
+        if (($('#edit-sitereporttype')[0].selectedIndex == 0) || ($('#edit-sitereporttype')[0].selectedIndex == 2)) { 
+          $('#edit-sitereporttype')[0].selectedIndex = 1;
+        }
+      }
+      $('#edit-sitereporttype option')[0].disabled = ($(this)[0].selectedIndex > 0);
+      $('#edit-sitereporttype option')[2].disabled = ($(this)[0].selectedIndex > 0);
+      return false;
+    });
+    // call change for initial settings
+    $(this).change();
+  });
+
+  /** Finder Page
+   *  If Roadside observation then dont select a Placetype
+   */
+  $('#edit-sitereporttype:not(.processed)').each(function() {
+    $(this).addClass('processed'); //this is for ourselves
+      // IE does not see the change event when it happens, so we help it a bit. FUCKERS!!
+      $(this).click(function() {
+        if ($.browser.msie) {
+          $(this).change();
+          $(this).blur();
+
+       }
+      });
+    $(this).change(function() {
+      $('#edit-placetype')[0].disabled = $(this)[0].selectedIndex == 2;
+      return false;
+    });
+    // call change for initial settings
+    $(this).change();
+  });
+  
+  
+
+  /** Add place page
+   *  If adding Roadside Observation, then hide placetype 
+   */
+  $('#devtrack-module-addplaceform .form-radio[name="reporttype"]:not(.processed)').each(function() {
+    $(this).addClass('processed'); //this is for ourselves
+      // IE does not see the change event when it happens, so we help it a bit. FUCKERS!!
+      $(this).click(function() {
+        if ($.browser.msie) {
+          $(this).change();
+          $(this).blur();
+
+       }
+      });
+      $(this).change(function() {
+        $('input[name="placetype"]').attr('disabled', ($(this)[0].value == 1 && $(this)[0].checked));
+        $('input[name="placetype"]').parent().parent().parent().parent().toggle(!($(this)[0].value == 1 && $(this)[0].checked));
+        $($('input[name="placetype"]')[4]).parent().toggle(!($(this)[0].value == 0 && $(this)[0].checked));
+        if ($('input[name="placetype"]')[4].checked) {
+          $('input[name="placetype"]')[3].checked = ($(this)[0].value == 0 && $(this)[0].checked);
+        }
+        return false;
+      });
+      // call change for initial settings
+      $(this)[0].checked = true;
+      
+      $(this).change();
+  });
+};
